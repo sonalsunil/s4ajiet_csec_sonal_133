066cc7369e55d4d0f9166812fac27a31004cafc9
sonalsunil <sonalsunil12345@gmail.com>
2026-05-07 11:52:40 +0530

    Create divide.java

---
+++ divide.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class divide{
+  public static void main (String [] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println(a/b);
+        System.out.println(a%b);
+}
+}
